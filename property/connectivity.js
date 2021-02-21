1fa7646e1021520770857ddbe383cdcb87bd6ee5
valclassdevelop <developervalclass@gmail.com>
2021-02-21 10:57:11 +0700

    membenarkan kesalahan kelas di component navbar main-toggle menjadi v-btn-toggle dan juga menambahkan pengkonisian if else di DOMnavbar.js

---
+++ property/connectivity.js
@@ -0,0 +1,8 @@
+for (var i = 0; i <= document.querySelectorAll('.v-connect').length; i++) {
+    if (navigator.onLine) {
+        document.querySelectorAll('.v-connect')[i].innerHTML = 'Online'
+
+    } else {
+        document.querySelectorAll('.v-connect')[i].innerHTML = 'Offline'
+    }
+}
