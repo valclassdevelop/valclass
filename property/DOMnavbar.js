abeba17d8647e0a9ff1d431dc5f2581f5dc243cd
valclassdevelop <developervalclass@gmail.com>
2021-02-21 17:12:45 +0700

    menghilangkan bug di navbar

--- property/DOMnavbar.js
+++ property/DOMnavbar.js
@@ -1,13 +1,13 @@
-if(document.querySelector('v-navbar')) {
+if (document.querySelector('.v-navbar')) {
 
     document.querySelector('.v-btn-toggle input').addEventListener('click', () => {
-        document.querySelector('v-navbar ul').classList.toggle('show')
+        document.querySelector('.v-navbar ul').classList.toggle('show')
     })
 }
 
-if(document.querySelector('v-navbar-fixed')) {
+if (document.querySelector('.v-navbar-fixed')) {
 
     document.querySelector('.v-btn-toggle input').addEventListener('click', () => {
-        document.querySelector('v-navbar-fixed ul').classList.toggle('show')
+        document.querySelector('.v-navbar-fixed ul').classList.toggle('show')
     })
 }
